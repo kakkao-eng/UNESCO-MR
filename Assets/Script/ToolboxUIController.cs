e5fdcd9e2ba9c50f195ef24ebf19b54c204c8d57
kakkao71 <kakkao73@gmail.com>
2025-07-07 15:53:09 +0700

    Make a game start system and equipment

---
+++ Assets/Script/ToolboxUIController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ToolboxUIController : MonoBehaviour
+{
+    public GameObject toolboxPanel; // Drag ToolboxPanel (World Space Canvas) มาวางใน Inspector
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T)) // กด T เพื่อเปิด/ปิด Toolbox
+            ToggleToolbox();
+    }
+
+    // เรียกฟังก์ชันนี้เมื่อกดปุ่ม/gesture/voice
+    public void ToggleToolbox()
+    {
+        if (toolboxPanel != null)
+            toolboxPanel.SetActive(!toolboxPanel.activeSelf);
+    }
+}
