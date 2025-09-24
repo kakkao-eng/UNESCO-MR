a9700c4a330ddfbeeb714084b038dc4697070481
QueqeQuiw <nattapat.suree@gmail.com>
2025-09-24 16:22:50 +0700

    add sound manager
    prepare to write code

---
+++ Assets/Script/SoundManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public enum AllSound
+    {
+        Background,
+        ClickButton,
+        WinGameSound,
+        LoseGameSound,
+        WarningTime,
+        WarningHitFossil,
+        Hammer,
+        Drill,
+        Brush,
+        Glue
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
